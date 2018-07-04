4f378b58de5eabf548b5519e547b2f51fa895158
Eix <eixxhis@gmail.com>
2018-07-04 16:05:24 -0400

    Add files via upload

---
+++ assets/help.lua
@@ -0,0 +1,21 @@
+local fnt = Font.load("app0:assets/helpfont.ttf") 
+while true do
+	Graphics.initBlend()
+	Screen.clear()
+	Font.print(fnt, 425, 0, "HELP MENU 0.1", Color.new(255, 255, 255)) 
+	Font.print(fnt, 10, 30, "BASIC FUNCTIONS", Color.new(255, 255, 255)) 
+	Font.print(fnt, 22, 60, "[] - OPEN OR CLOSE MENU", Color.new(255, 255, 255)) 
+	Font.print(fnt, 22, 75, "() - RUN A LUA FILE", Color.new(255, 255, 255)) 
+	Font.print(fnt, 28, 90, "X - RUN A SELF FILE", Color.new(255, 255, 255)) 
+	Font.print(fnt, 28, 120, "^ - SCROLL UP", Color.new(255, 255, 255)) 
+	Font.print(fnt, 28, 135, "> - SPEED SCROLL DOWN", Color.new(255, 255, 255)) 
+	Font.print(fnt, 28, 150, "v - SCROLL DOWN", Color.new(255, 255, 255)) 
+	Font.print(fnt, 28, 165, "< - SPEED SCROLL UP", Color.new(255, 255, 255)) 
+	Graphics.termBlend()
+	Screen.flip()
+	if Controls.check(Controls.read(), SCE_CTRL_SQUARE) then 
+    System.wait(800000)
+    Font.unload(fnt)
+		break 
+	end
+end
--- index.lua
+++ index.lua
@@ -1,74 +1,83 @@
 white = Color.new(255, 255, 255)
 cyan = Color.new(0, 255, 255)
-grey = Color.new(190, 190, 190)
 ::start::
-selfs = System.listDirectory("app0:exe/")
+selfs = System.listDirectory("app0:")
 image = Graphics.loadImage("app0:sce_sys/pic0.png")
 i = 1
 
 while true do
-	y = 75+(-i*20)
+   y = 75+(-i*20)
 
-	Graphics.initBlend()
-	Screen.clear()
-	Graphics.drawImage(0, 0, image)
-	Graphics.debugPrint(0, 25, "StemLoader v0.3 - Vita SELF/LUA Loader - /\ Delete, X SELF, O LUA", cyan)
-	
-	for j, file in pairs(selfs) do
-		x = 5
-		if i == j then
-			color = grey
-			x = 15
-		else
-			color = white
-		end
-		Graphics.debugPrint(x, y, file.name, color)
-		y = y + 20
-	end
-	Graphics.termBlend()
+   Graphics.initBlend()
+   Screen.clear()
+   Graphics.drawImage(0, 0, image)
+   Graphics.debugPrint(250, 25, "StemLoader v0.4 - Vita SELF/LUA Loader", white)
+   
+   for j, file in pairs(selfs) do
+      x = 5
+      if i == j then
+         color = cyan
+         x = 10
+      else
+         color = white
+      end
+      Graphics.debugPrint(x, y, file.name, color)
+      y = y + 20
+   end
+   Graphics.termBlend()
 
-	pad = Controls.read()
-	if Controls.check(pad, SCE_CTRL_CROSS) then
-		Graphics.initBlend()
-		Screen.clear()
-		Graphics.termBlend()
-		Screen.flip()
-		Graphics.initBlend()
-		Screen.clear()
-		Graphics.termBlend()
-		System.wait(800000)
-		System.launchEboot("app0:exe/" .. selfs[i].name)
-	elseif Controls.check(pad, SCE_CTRL_CIRCLE) then
-		Graphics.initBlend()
-		Screen.clear()
-		Graphics.termBlend()
-		Screen.flip()
-		Graphics.initBlend()
-		Screen.clear()
-		Graphics.termBlend()
-		System.wait(800000)
-		dofile("app0:exe/" .. selfs[i].name)
-	elseif Controls.check(pad, SCE_CTRL_TRIANGLE) then
-		System.deleteFile("ux0:app/STEM00001/exe/" .. selfs[i].name)
-		goto start
-	elseif Controls.check(pad, SCE_CTRL_UP) and not Controls.check(oldpad, SCE_CTRL_UP) then
-		i = i - 1
-	elseif Controls.check(pad, SCE_CTRL_DOWN) and not Controls.check(oldpad, SCE_CTRL_DOWN) then
-		i = i + 1
-	elseif Controls.check(pad, SCE_CTRL_LEFT) and not Controls.check(oldpad, SCE_CTRL_LEFT) then
-		i = i - 5
-	elseif Controls.check(pad, SCE_CTRL_RIGHT) and not Controls.check(oldpad, SCE_CTRL_RIGHT) then
-		i = i + 5
-	end
-	
-	if i > #selfs then
-		i = 1
-	elseif i < 1 then
-		i = #selfs
-	end
-	
-	-- Update oldpad and flip screen
-	oldpad = pad
-	Screen.flip()
-	
+   pad = Controls.read()
+   if Controls.check(pad, SCE_CTRL_CROSS) then
+      Graphics.initBlend()
+      Screen.clear()
+      Graphics.termBlend()
+      Screen.flip()
+      Graphics.initBlend()
+      Screen.clear()
+      Graphics.termBlend()
+      System.wait(800000)
+      System.launchEboot("app0:" .. selfs[i].name)
+   elseif Controls.check(pad, SCE_CTRL_CIRCLE) then
+      Graphics.initBlend()
+      Screen.clear()
+      Graphics.termBlend()
+      Screen.flip()
+      Graphics.initBlend()
+      Screen.clear()
+      Graphics.termBlend()
+      System.wait(800000)
+      dofile("app0:" .. selfs[i].name)
+   elseif Controls.check(pad, SCE_CTRL_SQUARE) then
+      Graphics.initBlend()
+      Screen.clear()
+      Graphics.termBlend()
+      Screen.flip()
+      Graphics.initBlend()
+      Screen.clear()
+      Graphics.termBlend()
+      System.wait(800000)
+      dofile("app0:assets/help.lua")
+   elseif Controls.check(pad, SCE_CTRL_TRIANGLE) then
+      System.deleteFile("ux0:app/STEM00001/" .. selfs[i].name)
+      goto start
+   elseif Controls.check(pad, SCE_CTRL_UP) and not Controls.check(oldpad, SCE_CTRL_UP) then
+      i = i - 1
+   elseif Controls.check(pad, SCE_CTRL_DOWN) and not Controls.check(oldpad, SCE_CTRL_DOWN) then
+      i = i + 1
+   elseif Controls.check(pad, SCE_CTRL_LEFT) and not Controls.check(oldpad, SCE_CTRL_LEFT) then
+      i = i - 5
+   elseif Controls.check(pad, SCE_CTRL_RIGHT) and not Controls.check(oldpad, SCE_CTRL_RIGHT) then
+      i = i + 5
+   end
+   
+   if i > #selfs then
+      i = 1
+   elseif i < 1 then
+      i = #selfs
+   end
+   
+   -- Update oldpad and flip screen
+   oldpad = pad
+   Screen.flip()
+   
 end
